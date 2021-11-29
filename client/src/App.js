cea36b85c5994793564e601c4611521d509ae206
ksheeraj1161 <kandra.ksheeraj@gmail.com>
2021-11-29 11:56:59 +0530

    working commit

---
+++ client/src/App.js
@@ -0,0 +1,85 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
+function App() {
+  const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [passwordList, setPasswordList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/showpasswords").then((response) => {
+      setPasswordList(response.data);
+    });
+  }, []);
+
+  const addPassword = () => {
+    Axios.post("http://localhost:3001/addpassword", {
+      password: password,
+      title: title,
+    });
+  };
+
+  const decryptPassword = (encryption) => {
+    Axios.post("http://localhost:3001/decryptpassword", {
+      password: encryption.password,
+      iv: encryption.iv,
+    }).then((response) => {
+      setPasswordList(
+        passwordList.map((val) => {
+          return val.id == encryption.id
+            ? {
+                id: val.id,
+                password: val.password,
+                title: response.data,
+                iv: val.iv,
+              }
+            : val;
+        })
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="AddingPassword">
+          <input
+          type="text"
+          placeholder="Title"
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+          <input
+          type="text"
+          placeholder="Password / ID number"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <button onClick={addPassword}> Add to Inventory</button>
+      </div>
+
+      <div className="Passwords">
+        {passwordList.map((val, key) => {
+          return (
+            <div
+              className="password"
+              onClick={() => {
+                decryptPassword({
+                  password: val.password,
+                  iv: val.iv,
+                  id: val.id,
+                });
+              }}
+              key={key}
+            >
+              <h3>{val.title}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
